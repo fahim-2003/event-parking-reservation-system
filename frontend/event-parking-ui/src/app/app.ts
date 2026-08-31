4b350790324c24441ef87582a9eddcc047fe06df
fahim-2003 <mohamed.fahim.kul3@gmail.com>
2026-08-31 10:53:13 +0530

    chore: establish Angular Tailwind frontend foundation

---
+++ frontend/event-parking-ui/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('event-parking-ui');
+}
